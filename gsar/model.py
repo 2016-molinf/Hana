4e5839ed5d00873132144224329162023416c7d1
hannicka12 <hannicka12@seznam.cz>
2016-05-22 17:03:37 +0200

    Kontrola testovacich dat
    Přidání kontroly, zda jsou data vhodná pro můj model

--- gsar/model.py
+++ gsar/model.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from rdkit import Chem
 from rdkit.Chem import Descriptors
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
 
 from sklearn import cross_validation
 from sklearn.grid_search import GridSearchCV
@@ -13,6 +15,8 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
+
+
 
 def gsarIC50(soubor, treshold=1000, test_size=0.3):
     # nacteni dat
@@ -29,7 +33,12 @@ def gsarIC50(soubor, treshold=1000, test_size=0.3):
 
         print("Create dataset")
         print("...")
+        # Zobrazeni rozlozneni hodnot IC50
         print(data.STANDARD_VALUE.hist())
+        plt.title("Standard values distribution")
+        plt.xlabel('IC50')
+
+        #Vytvoreni datasetu
         data["MOL"] = data.CANONICAL_SMILES.apply(Chem.MolFromSmiles)
         dataset = data[["MOL", "STANDARD_VALUE"]]
 
@@ -55,8 +64,23 @@ def gsarIC50(soubor, treshold=1000, test_size=0.3):
         print("Split into a training and testing set %d x %d"
             % (100-(test_size*100), (test_size*100)))
         train, test = cross_validation.train_test_split(dataset, test_size=test_size, random_state=1)
-        print(len(train), len(test))
+        #print(len(train), len(test))
         print("...")
+
+        #spocitani fingerprints a prumerne vzdalenosti fingerprints
+        fps = [FingerprintMols.FingerprintMol(x) for x in train["MOL"]]
+        avgValue = 0
+
+        def distij(i,j,fps=fps):
+            return DataStructs.FingerprintSimilarity(fps[i],fps[j])
+
+        for i in range(len(fps)):
+            for j in range(len(fps)):
+                avgValue += distij(i,j,fps)
+
+        avgValue /= len(fps)**2
+        #print("prumer",p, len(fps)**2,avgValue)
+
 
         # ulozeni trenovaci a testovaci mnoziny
         train.to_csv('train.csv',sep='\t', index=False)
@@ -69,17 +93,17 @@ def gsarIC50(soubor, treshold=1000, test_size=0.3):
         train["ACTIVITY_COMMENT"] = train.STANDARD_VALUE < treshold
         test["ACTIVITY_COMMENT"] = test.STANDARD_VALUE < treshold
         """
-        
+
         # fce pro vztvoreni vstupnich dat do jednotlivzch modelu
         def getX(dataset):
-                return np.array(dataset[list(dataset.columns[2:])])
+            return np.array(dataset[list(dataset.columns[2:])])
         def gety(dataset):
-                return np.array(dataset.ACTIVITY_COMMENT)
-        # rozdeleni na trenovaci a testovaci data
+            return np.array(dataset.ACTIVITY_COMMENT)
+
+        # rozdeleni na data a tridy
         X_train = getX(train)
-        X_test = getX(test)
         y_train = gety(train)
-        y_test = gety(test)
+
 
         # vypocet PCA
         n_samples, n_features = X_train.shape
@@ -93,7 +117,6 @@ def gsarIC50(soubor, treshold=1000, test_size=0.3):
 
         # transformace dat pro zvoleny pocet komponent
         X_train_pca = pca.transform(X_train)
-        X_test_pca = pca.transform(X_test)
 
         # trenovani SVM modelu
         print("Fitting the classifier to the training set")
@@ -105,11 +128,25 @@ def gsarIC50(soubor, treshold=1000, test_size=0.3):
         print("Best estimator found by grid search:")
         print(clf.best_estimator_)
 
-        # Evaluace modelu
         print("...")
         print("Predicting activity of compounds")
+        # Urceni yda jsou data vhodna pro nas model, yda jsou podobna trenovacim datum
+        testValue = 0
+        tfps = [FingerprintMols.FingerprintMol(x) for x in test["MOL"]]
+        def tdistij(i,j,fps,tfps):
+            return DataStructs.FingerprintSimilarity(tfps[i],fps[j])
+        for i in range(len(test["MOL"])):
+            for j in range(len(fps)):
+                testValue += tdistij(i,j,fps,tfps)
+            testValue /= len(fps)
+            if testValue < (avgValue / 4):
+                test = test.drop(test.index[[i]])
+        X_test = getX(test)
+        y_test = gety(test)
+        X_test_pca = pca.transform(X_test)
         y_pred = clf.predict(X_test_pca)
 
+        # Evaluace modelu
         target_names = ["Not Active","Active"]
         n_classes = 2
         print(classification_report(y_test, y_pred, target_names=target_names))
